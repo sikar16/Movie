3061d126714d8486114b2819b433bc8f04ef0d60
sikar16 <sikarmahi16@gmail.com>
2024-06-28 07:05:07 +0300

    add some component

--- Movie app/src/App.jsx
+++ Movie app/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './MarkUp/Component/Header/Header';
 import HomePage from './MarkUp/HomePage/HomePage';
 import Footer from './MarkUp/Component/Footer/Footer';
+import SinglePage from './MarkUp/Component/Watch/SinglePage';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/SinglePage/:id" element={<SinglePage/>} />
       </Routes>
       <Footer />
     </Router>
--- Movie app/src/MarkUp/Component/Watch/SinglePage.jsx
+++ Movie app/src/MarkUp/Component/Watch/SinglePage.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './singlepage.css'
+import { useParams } from 'react-router-dom'
+import { homeData, recommended } from '../../../DammpyData'
+import Upcomming from '../Upcomming/Upcomming'
 
 function SinglePage() {
+    const{id}=useParams()
+    const[item,setitems]=useState(null)
+    
+    useEffect(()=>{
+        let item=homeData.find((item)=>item.id===parseInt(id))
+        if(item){
+            setitems(item)
+        }
+    },[id])
+    const[rec,setrec]=useState(recommended)
+
   return (
-    <div>SinglePage</div>
+    <>{item? (<>
+    <section>
+        <div className="singlepage">
+        <div className="singHeading">
+            <h1>{item.name}</h1>
+            <span>| {item.time} | Hd</span>
+        </div>
+        <div className="container">
+            <video src={item.video} controls></video>
+            <div className="para">
+                <h3>
+                    {item.date}
+                </h3>
+                <p>
+                    {item.desc}
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut harum dicta sit veritatis explicabo architecto velit dignissimos excepturi ducimus. Assumenda cum officia fugit debitis labore, cupiditate veritatis dolorem. Ad, porro!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut harum dicta sit veritatis explicabo architecto velit dignissimos excepturi ducimus. Assumenda cum officia fugit debitis labore, cupiditate veritatis dolorem. Ad, porro!</p>
+            </div>
+            <div className="social">
+                <h3>Share</h3>
+                <i className='fab fa-facebook-f'></i>
+                <i className='fab fa-twitter'></i>
+                <i className='fab fa-youtube'></i>         
+                   </div>
+        </div>
+    </div>
+    </section>
+    <Upcomming items={rec} title='Recommanded Movies'/>
+    
+    </>):null}</>
   )
 }
 
